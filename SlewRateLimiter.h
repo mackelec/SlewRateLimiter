4a323cbfc86c7c8279d1b62dee73642920f02fa4
mackelec <mackelec@users.noreply.github.com>
2023-11-03 08:23:08 +1000

    Update SlewRateLimiter.h

--- SlewRateLimiter.h
+++ SlewRateLimiter.h
@@ -43,10 +43,16 @@
 
 #include "Arduino.h"
 
-enum SmoothingExponent 
-{
+enum SmoothingExponent {
     SMOOTHING_VALUE_1 = 0,
-    // ... (rest of the smoothing values)
+    SMOOTHING_VALUE_2 = 1,
+    SMOOTHING_VALUE_4 = 2,
+    SMOOTHING_VALUE_8 = 3,
+    SMOOTHING_VALUE_16 = 4,
+    SMOOTHING_VALUE_32 = 5,
+    SMOOTHING_VALUE_64 = 6,
+    SMOOTHING_VALUE_128 = 7,
+    SMOOTHING_VALUE_256 = 8,
     SMOOTHING_VALUE_512 = 9
 };
 
