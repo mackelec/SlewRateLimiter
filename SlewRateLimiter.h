023e916a01139c33339be7b3fb4220252eb45a64
mackelec <mackelec@users.noreply.github.com>
2023-11-03 08:21:43 +1000

    Update SlewRateLimiter.h

--- SlewRateLimiter.h
+++ SlewRateLimiter.h
@@ -1,26 +1,42 @@
 /**
  * @file SlewRateLimiter.h
- * @brief Provides fixed and adaptive slew rate limiting using an Exponential Moving Average (EMA) calculation.
+ * @brief An Arduino library for limiting the rate of change of a signal (slew rate control).
  *
- * The SlewRateLimiter class is designed to control the rate of change of a signal, which is 
- * particularly useful for preventing abrupt changes that may lead to overshoot, instability, or 
- * undesirable system behavior. It features both fixed and adaptive modes for slew rate limiting, 
- * with the adaptive mode allowing for greater changes in the signal when the difference between 
- * the current input and the EMA is large. This class also includes a hysteresis feature to prevent 
- * minor fluctuations within a specified band, enhancing the stability and smoothness of the output signal.
+ * The SlewRateLimiter class provides a mechanism for imposing a maximum rate of change on a signal. 
+ * It implements an Exponential Moving Average (EMA) for signal smoothing and applies rate limiting to 
+ * the changes in the output signal to prevent overshoot, ensure stability, and promote a smooth response.
+ * The limiter can operate in a fixed mode, where the rate of change is constant, or an adaptive mode,
+ * which allows the rate of change to increase with larger signal deviations, providing a responsive yet
+ * stable control system.
  *
- * Usage:
- * - Create an instance of SlewRateLimiter, optionally specifying the smoothing exponent, fixed rate limit, 
- *   hysteresis band, and adaptive slope.
- * - Use the processValue method to input a new value and receive the rate-limited output.
- * - Adjust the rate limiting, hysteresis, and smoothing parameters at runtime if needed.
+ * Major features:
+ * - Fixed slew rate limiting: A constant maximum change is allowed between successive output values.
+ * - Adaptive slew rate limiting: The rate of change is allowed to increase with larger input deviations.
+ * - Hysteresis: Prevents changes to the output when the input changes are within a certain range, reducing noise.
+ * - EMA Smoothing: Smooths out the input signal fluctuations using an Exponential Moving Average.
  *
- * @note The adaptive slope is internally scaled to a base of 128 to utilize efficient bit-shifting operations 
- *       during calculations, reducing processing overhead.
+ * Major methods:
+ * - processValue: Processes an input value and returns the limited output.
+ * - setRateLimit: Sets the fixed rate limit.
+ * - setHysteresisBand: Sets the width of the hysteresis band.
+ * - setSmoothingExponent: Sets the exponent used for EMA calculation.
+ * - setAdaptiveSlope: Sets the slope for adaptive rate limiting.
+ * - reset: Resets the EMA and last output value.
+ *
+ * Major variables:
+ * - lastValue: The last output value after rate limiting and hysteresis.
+ * - emaValue: The current value of the Exponential Moving Average.
+ * - currentExponent: The exponent used for the EMA calculation.
+ * - rateLimit: The maximum allowed change per output update in fixed mode.
+ * - hysteresisBand: The range within which output changes are suppressed to reduce noise.
+ * - adaptiveSlopeInternal: The factor by which the rate limit increases with larger input deviations.
+ *
+ * @note This library is designed to be efficient enough for use in real-time systems, such as those based on Arduino.
  *
  * @author  Andrew McKinnon
  * @date    2023-11-3
  */
+
 
 #ifndef SlewRateLimiter_h
 #define SlewRateLimiter_h
